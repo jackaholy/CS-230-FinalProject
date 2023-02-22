2d6052051f130ba1e161d1934586a053f91bff60
jackaholy <98115075+jackaholy@users.noreply.github.com>
2023-02-22 11:03:41 -0700

    Create WaterBug.java

---
+++ src/final_project/WaterBug.java
@@ -0,0 +1,15 @@
+package final_project;
+
+import javax.swing.JFrame;
+
+/**
+ * This class will be our level 1 boat.
+ */
+public class WaterBug extends Player{
+
+	public WaterBug(JFrame playerJFrame) {
+		super(playerJFrame);
+		// TODO Auto-generated constructor stub
+	}
+
+}
