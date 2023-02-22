acbc902717be76f8dedbbd11cb84732f0c49645f
Justin <jforseth210@gmail.com>
2023-02-22 10:31:56 -0700

    Create super basic window

--- src/final_project/GameController.java
+++ src/final_project/GameController.java
@@ -1,10 +1,17 @@
 package final_project;
 
+import javax.swing.JFrame;
+
 /**
  * The main file of the project. Run this one to start the project
  */
 public class GameController {
     public static void main(String[] args) {
-        // TODO: Make game
+        new GameController();
+    }
+
+    public GameController() {
+        JFrame gameJFrame = new JFrame();
+        gameJFrame.setVisible(true);
     }
 }
