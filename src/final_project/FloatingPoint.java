dd6c0f7b795847be8a54b14bcc04f4a110fed459
jackaholy <98115075+jackaholy@users.noreply.github.com>
2023-02-22 11:04:53 -0700

    Create FloatingPoint.java

---
+++ src/final_project/FloatingPoint.java
@@ -0,0 +1,15 @@
+package final_project;
+
+import javax.swing.JFrame;
+
+/**
+ * This class will be our level 2 boat.
+ */
+public class FloatingPoint extends Player{
+
+	public FloatingPoint(JFrame playerJFrame) {
+		super(playerJFrame);
+		// TODO Auto-generated constructor stub
+	}
+
+}
