536f3bc5f49ee4c23be190e0ec311be4670fed0d
Justin <jforseth210@gmail.com>
2023-02-22 11:55:44 -0700

    Move some logic from player to entity. Remove some dolphin logic for now

--- src/final_project/Sprite.java
+++ src/final_project/Sprite.java
@@ -1,9 +1,27 @@
 package final_project;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 /**
  * A "thing" in the game. Should handle some basic stuff. Show/hide, movement,
  * collisions, etc.
  */
-public class Sprite {
+abstract public class Sprite {
+    private final JLabel spriteJLabel = new JLabel();
+    private ImageIcon image;
+    protected int x;
+    protected int y;
 
+    public Sprite(ImageIcon image, int x, int y) {
+        this.image = image;
+        this.x = x;
+        this.y = y;
+    }
+
+    protected void draw() {
+        spriteJLabel.setIcon(image);
+        spriteJLabel.setBounds(x, y, image.getIconWidth(), image.getIconHeight());
+        spriteJLabel.setVisible(true);
+    }
 }
