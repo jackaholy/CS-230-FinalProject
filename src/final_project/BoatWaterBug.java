bc3a79af3155c9168ca69ef1cc75acf31b94a8ef
jackaholy <98115075+jackaholy@users.noreply.github.com>
2023-02-22 11:06:24 -0700

    Update and rename WaterBug.java to BoatWaterBug.java

--- src/final_project/BoatWaterBug.java
+++ src/final_project/BoatWaterBug.java
@@ -5,9 +5,9 @@ import javax.swing.JFrame;
 /**
  * This class will be our level 1 boat.
  */
-public class WaterBug extends Player{
+public class BoatWaterBug extends Player{
 
-	public WaterBug(JFrame playerJFrame) {
+	public BoatWaterBug(JFrame playerJFrame) {
 		super(playerJFrame);
 		// TODO Auto-generated constructor stub
 	}
