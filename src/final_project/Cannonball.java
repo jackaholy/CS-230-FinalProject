fc72bd10351fb8d0d9c6fb6619d0d29619d76b7b
Justin <jforseth210@gmail.com>
2023-03-22 11:25:48 -0600

    Rip out a bunch of poorly thought out code

--- src/final_project/Cannonball.java
+++ src/final_project/Cannonball.java
@@ -10,21 +10,15 @@ public class Cannonball extends MovingSprite {
     public static final double SPEED = 1.5;
     public static final ImageIcon ICON = new ImageIcon("assets/cannonball.png");
 
-    private final long timeOfImpact;
     private boolean isFlying = true;
 
     public Cannonball(JFrame gameJFrame, int x, int y, double angle) {
-        super(gameJFrame, ICON, x, y, speed, 0);
+        super(gameJFrame, ICON, x, y, SPEED, 0);
         setRotation(angle);
-        timeOfImpact = System.currentTimeMillis() + FLIGHT_TIME_MS;
     }
 
     @Override
     public void tick() {
-        if (System.currentTimeMillis() > timeOfImpact) {
-            isFlying = false;
-            return;
-        }
         Image image = ICON.getImage(); // transform it
         Image newimg = image.getScaledInstance(getWidth() + 1, getHeight() + 1, java.awt.Image.SCALE_SMOOTH); // scale
                                                                                                               // it the
