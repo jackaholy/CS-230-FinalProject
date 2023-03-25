bcb0d884748bfe45c9861f584ee536d7f5ac1463
Jack Holy <jholy@carroll.edu>
2023-03-24 18:32:03 -0600

    Add background image and buttons

--- src/final_project/TitleScreen.java
+++ src/final_project/TitleScreen.java
@@ -1,47 +1,73 @@
 package final_project;
 
+import java.awt.Color;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.WindowConstants;
+
 
 /**
  * Create a title screen
  */
 public class TitleScreen extends JFrame {
 
-    protected TitleScreen(JFrame gameJFrame) {
+    protected TitleScreen() {
+	setTitle("Virtual Voyagers");
 
 	JLabel backgroundJLabel = new JLabel();
 
 	ImageIcon backgroundPicture = new ImageIcon("assets/tropical_titlescreen.jpeg");
 	backgroundJLabel.setIcon(backgroundPicture);
 	backgroundJLabel.setBounds(0, 0, backgroundPicture.getIconWidth(), backgroundPicture.getIconHeight());
-	gameJFrame.getContentPane().add(backgroundJLabel);
+	getContentPane().add(backgroundJLabel);
 
-	backgroundJLabel.setVisible(true);
-
-	gameJFrame = new JFrame("Virtual Voyagers");
-
-	gameJFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	JButton playBtn = new JButton("Play");
+	playBtn.addMouseListener(new MouseAdapter() {
+	    
+	    public void mouseClicked(MouseEvent e) {
+		// If pressed, continue to the game
+	    }
+	});
+	playBtn.setBackground(Color.BLACK);
+	
+	playBtn.setBounds(325, 350, 100, 30);
+	backgroundJLabel.add(playBtn);
+	
+	JButton quitBtn = new JButton("Quit");
+	quitBtn.addMouseListener(new MouseAdapter() {
+	    
+	    public void mouseClicked(MouseEvent e) {
+		dispose();
+	    }
+	});
+	quitBtn.setBackground(Color.BLACK);
+	
+	quitBtn.setBounds(325, 400, 100, 30);
+	backgroundJLabel.add(quitBtn);
+	
 
 	// Get the size of the screen
 	GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 	int width = gd.getDisplayMode().getWidth();
 	int height = gd.getDisplayMode().getHeight();
 	// Set the size of the title screen
-	gameJFrame.setSize((width / 2) + 50, (height / 2) + 150);
-
+	setSize((width / 2) + 50, (height / 2) + 150);
+	
+	setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	// Put the window in the middle of the screen.
-	gameJFrame.setLocationRelativeTo(null);
+	setLocationRelativeTo(null);
 	// Make it visible
-	gameJFrame.setVisible(true);
+	setVisible(true);
     }
 
     public static void main(String[] args) {
-	JFrame frame = new JFrame();
-	new TitleScreen(frame);
+	new TitleScreen();
     }
 }
