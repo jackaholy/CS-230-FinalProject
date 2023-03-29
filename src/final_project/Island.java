d149efe68722813a5d3b421f64dc6473bc4f46f5
Jack Holy <jholy@carroll.edu>
2023-03-29 10:48:44 -0600

    Remove unused method

--- src/final_project/Island.java
+++ src/final_project/Island.java
@@ -8,26 +8,12 @@ import javax.swing.JFrame;
  */
 public class Island extends Sprite {
     
-//    {"../assets/island_down.png", "../assets/island_up.png"};
-    ImageIcon[] img = new ImageIcon[3];
-    
-
     protected Island(JFrame gameJFrame, ImageIcon image) {
 	super(gameJFrame, image, 0, 0);
 	
 	 // Subtract 40 so islands don't spawn too close to the content pane border.
 	this.x = getRandomX(20, gameJFrame.getContentPane().getWidth() - 20);
         this.y = getRandomY(20, gameJFrame.getContentPane().getHeight() - 20);
-    }
-
-    /**
-     * Set the x coordinate of the loot to a random x value in the window.
-     * 
-     * @param min - the minimum x coordinate value that loot could spawn
-     * @param max - the maximum x coordinate value that loot could spawn
-     */
-    private ImageIcon[] getRandomIslandImage(ImageIcon image) {
-       return img;
     }
     
     /**
