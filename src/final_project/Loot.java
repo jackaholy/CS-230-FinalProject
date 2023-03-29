603d392d1ce86ceefa2a1f9a8d632b68a4ac7189
Jack Holy <jholy@carroll.edu>
2023-03-29 10:32:17 -0600

    Add some comments and JavaDoc

--- src/final_project/Loot.java
+++ src/final_project/Loot.java
@@ -5,29 +5,31 @@ import javax.swing.JFrame;
 
 /**
  * Loot that spawns on map. Each loot has a randomly assigned x and y
- * coordinate.
+ * coordinate. When the player moves over loot their money count increases by
+ * one, and the loot image disappears.
  */
 
 public class Loot extends Sprite {
-    // boolean flag to determine if loot has already been collected
+
+    // Boolean flag to determine if loot has already been collected
     private boolean collected = false;
 
     /**
-     * Create loot with random coordinates
+     * Create loot with random coordinates.
      * 
      * @param gameJFrame the window to add the loot to
      * @param image      the image of the loot
      */
     protected Loot(JFrame gameJFrame, ImageIcon image) {
-        super(gameJFrame, image, 0, 0);
+	super(gameJFrame, image, 0, 0);
 
-        // Subtract 20 so loot doesn't spawn too close to the content pane border.
-        this.x = getRandomX(20, gameJFrame.getContentPane().getWidth() - 20);
-        this.y = getRandomY(20, gameJFrame.getContentPane().getHeight() - 20);
+	// Subtract 20 so loot doesn't spawn too close to the content pane border.
+	this.x = getRandomX(20, gameJFrame.getContentPane().getWidth() - 20);
+	this.y = getRandomY(20, gameJFrame.getContentPane().getHeight() - 20);
     }
 
     /**
-     * Create loot with specific coordinates
+     * Create loot with specific coordinates.
      * 
      * @param gameJFrame the window to add the loot to
      * @param image      the image of the loot
@@ -35,53 +37,52 @@ public class Loot extends Sprite {
      * @param y          the y coordinate the loot will spawn at
      */
     protected Loot(JFrame gameJFrame, ImageIcon image, int x, int y) {
-        super(gameJFrame, image, x, y);
+	super(gameJFrame, image, x, y);
     }
 
     /**
      * Set the x coordinate of the loot to a random x value in the window.
      * 
-     * @param min - the minimum x coordinate value that loot could spawn
-     * @param max - the maximum x coordinate value that loot could spawn
+     * @param min the minimum x coordinate value that loot could spawn
+     * @param max the maximum x coordinate value that loot could spawn
      */
     private int getRandomX(int min, int max) {
-        return (int) ((Math.random() * (max - min)) + min);
+	return (int) ((Math.random() * (max - min)) + min);
     }
 
     /**
      * Set the y coordinate of the loot to a random y value in the window.
      * 
-     * @param min - the minimum y coordinate value that loot could spawn
-     * @param max - the maximum y coordinate value that loot could spawn
+     * @param min the minimum y coordinate value that loot could spawn
+     * @param max the maximum y coordinate value that loot could spawn
      */
     private int getRandomY(int min, int max) {
-        return (int) ((Math.random() * (max - min)) + min);
+	return (int) ((Math.random() * (max - min)) + min);
     }
 
     /**
      * Is called when the player ship overlaps with the loot objects.
      * 
-     * @param player - the player's boat.
-     * @return true if loot is collected by player ship.
+     * @param player the player's boat
+     * @return boolean true if loot is collected by player ship. Otherwise return
+     *         false
      */
     protected boolean isCollected(PlayerShip player, int padding) {
-        if (!collected && x - padding <= player.getX()
-                && player.getX() <= (x + this.getWidth() + padding)
-                && (y - padding <= player.getY()
-                        && player.getY() <= (y + this.getHeight() + padding))) {
-            collected = true;
-            return true;
-        }
-        return false;
+	if (!collected && x - padding <= player.getX() && player.getX() <= (x + this.getWidth() + padding)
+		&& (y - padding <= player.getY() && player.getY() <= (y + this.getHeight() + padding))) {
+	    collected = true;
+	    return true;
+	}
+	return false;
     }
 
     /**
+     * If the player ship overlaps with the loot objects, erase them from the
+     * JFrame.
      * 
-     * @param gameJFrame
-     * @return true if the player collects loot.
+     * @param gameJFrame the window to remove the loot from
      */
-    protected boolean collect(JFrame gameJFrame) {
-        gameJFrame.getContentPane().remove(spriteJLabel);
-        return true;
+    protected void collect(JFrame gameJFrame) {
+	gameJFrame.getContentPane().remove(spriteJLabel);
     }
 }
