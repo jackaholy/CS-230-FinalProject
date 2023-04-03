b88723e64921cda1128cd1a63bd19429d71c6c6b
Justin <jforseth210@gmail.com>
2023-04-03 11:44:11 -0600

    Get started writing tests for MovingSprite

---
+++ src/testing/TestEverything.java
@@ -0,0 +1,6 @@
+public class TestEverything {
+    public static void main(String[] args) {
+        TestSprite.main(args);
+        TestMovingSprite.main(args);
+    }
+}
---
+++ src/testing/TestMovingSprite.java
@@ -0,0 +1,5 @@
+public class TestMovingSprite {
+    public static void main(String[] args) {
+
+    }
+}
