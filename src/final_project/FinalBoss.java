90ca3c722f17fce6f28d48a4d890df143057e07b
Justin <jforseth210@gmail.com>
2023-04-05 11:30:44 -0600

    Add final boss

---
+++ src/final_project/FinalBoss.java
@@ -0,0 +1,14 @@
+package final_project;
+
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class FinalBoss extends PirateShip {
+    public FinalBoss(JFrame gameJFrame, List<Loot> lootList, double speed,
+            double turningSpeed, int health,
+            int turnDistance) {
+        super(gameJFrame, new ImageIcon("assets/boss.png"), lootList, speed, turningSpeed, health, turnDistance);
+    }
+}
