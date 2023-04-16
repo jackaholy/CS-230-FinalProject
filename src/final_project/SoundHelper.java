f5c6548e84cae3081ed9b4378d3d000ac2f7bc07
Justin Forseth <jforseth210@gmail.com>
2023-04-16 15:50:37 -0600

    Mostly working threaded...

--- src/final_project/SoundHelper.java
+++ src/final_project/SoundHelper.java
@@ -14,21 +14,31 @@ public class SoundHelper {
     };
 
     public static void playSound(String sound) {
-        try {
-            // Open an audio input stream from the sound file
-            File soundFile = new File("assets/sound/" + sound);
-            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+        Thread audioThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                try {
+                    // Open an audio input stream from the sound file
+                    File soundFile = new File("assets/sound/" + sound);
+                    AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
 
-            // Get a sound clip resource from the audio input stream
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioIn);
+                    // Get a sound clip resource from the audio input stream
+                    Clip clip = AudioSystem.getClip();
+                    clip.open(audioIn);
 
-            // Play the sound clip
-            clip.start();
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+                    // Play the sound clip
+                    clip.start();
+                    Thread.sleep(clip.getMicrosecondLength() / 100);
+                    clip.close();
+                    audioIn.close();
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
+        audioThread.start();
     }
 
     public static void playSoundBackground(String sound) {
